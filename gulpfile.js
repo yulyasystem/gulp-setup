edfd0adcf9620f783ad5ea99a115e36426af81e2
yulyasystem <yulyasystem@gmail.com>
2019-02-08 00:30:19 +0200

    update build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const inject = require('gulp-inject');
+const webserver = require('gulp-webserver');
 
-const PATHS = {
+
+
+
+
+
+const PATH = {
   src: 'src/**/*',
   srcHTML: 'src/**/*.html',
   srcCSS: 'src/**/*.css',
@@ -13,15 +21,51 @@ const PATHS = {
 };
 
 gulp.task('html', function () {
-  return gulp.src(PATHS.srcHTML).pipe(gulp.dest(PATHS.build));
+  return gulp.src(PATH.srcHTML).pipe(gulp.dest(PATH.build));
 });
 
 gulp.task('css', function () {
-  return gulp.src(PATHS.srcCSS).pipe(gulp.dest(PATHS.build));
+  return gulp.src(PATH.srcCSS).pipe(gulp.dest(PATH.build));
 });
 
 gulp.task('js', function () {
-  return gulp.src(PATHS.srcJS).pipe(gulp.dest(PATHS.build));
+  return gulp.src(PATH.srcJS).pipe(gulp.dest(PATH.build));
 });
 
+
+
 gulp.task('copy', gulp.series(['html', 'css', 'js']));
+
+gulp.task('inject', gulp.series(['copy'], function () {
+  let css = gulp.src(PATH.buildCSS);
+  let js = gulp.src(PATH.buildJS);
+  return gulp.src(PATH.buildIndex)
+    .pipe(inject(css, {
+      relative: true
+    }))
+    .pipe(inject(js, {
+      relative: true
+    }))
+    .pipe(gulp.dest(PATH.build));
+}));
+
+
+
+gulp.task('serve',gulp.series( ['inject'], function () {
+  return gulp.src(PATH.build)
+    .pipe(webserver({
+      port: 3000,
+      livereload: true
+    }));
+}));
+
+
+
+gulp.task('watch', gulp.series(['serve'], function () {
+  gulp.watch(PATH.src, gulp.series(['inject']));
+}));
+
+
+gulp.task('default', gulp.parallel(['watch']));
+
+
